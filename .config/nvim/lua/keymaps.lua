c8365f000f483e7f97765d345c436b317535a76d
Phil Gyford <phil@gyford.com>
2026-01-21 09:57:29 +0000

    Move autocmd

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -77,23 +77,7 @@ for lhs, map in pairs(maps) do
     desc = map.desc,
   })
 end
-
--- Close Lazy.nvim window with ESC key (otherwise it's :close)
-local user_grp = vim.api.nvim_create_augroup('LazyUserGroup', { clear = true })
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'lazy',
-  desc = 'Quit lazy with <esc>',
-  callback = function()
-    vim.keymap.set(
-      'n',
-      '<esc>',
-      function() vim.api.nvim_win_close(0, false) end,
-      { buffer = true, nowait = true }
-    )
-  end,
-  group = user_grp,
-})
-
+)
 
 
 
