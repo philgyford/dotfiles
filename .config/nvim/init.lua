8fea129b74d89abde15879b04a7d4935c513538b
Phil Gyford <phil@gyford.com>
2026-01-31 15:13:22 +0000

    Add comments

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,10 @@ vim.g.mapleader = ","
 
 
 if vim.g.vscode then
-	require("init-vscode")
+  -- For when we're using the Neovim plugin in VS Code,
+  -- use the one config file that's for that:
+  require("init-vscode")
 else
-	require("init")
+  -- For all other Neovims, everything else:
+  require("init")
 end
