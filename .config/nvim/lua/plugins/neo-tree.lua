33c67962350de5a8fb5de1da4bbd9c097f00f2ad
Phil Gyford <phil@gyford.com>
2026-02-03 11:30:22 +0000

    Show most hidden files in neo-tree by default

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,25 +1,37 @@
 return {
-  {
-    'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v3.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'MunifTanjim/nui.nvim',
-      'nvim-tree/nvim-web-devicons', -- optional, but recommended
-    },
-    lazy = false, -- neo-tree will lazily load itself
-    config = function()
-      require('neo-tree').setup({
-        window = {
-          width = 35,
-        },
-      })
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons", -- optional, but recommended
+		},
+		lazy = false, -- neo-tree will lazily load itself
+		config = function()
+			require("neo-tree").setup({
+				filesystem = {
+					filtered_items = {
+						visible = true, -- show hidden files by default (H to toggle)
+						never_show = {
+							".DS_Store",
+							".git",
+						},
+						never_show_by_pattern = {
+							"*.un~",
+						},
+					},
+				},
+				window = {
+					width = 35,
+				},
+			})
 
-      vim.keymap.set("n", "<leader>d", "<cmd>Neotree toggle<CR>", {
-        noremap = true,
-        silent = true,
-        desc = "Toggle Neo-tree",
-      })
-    end,
-  }
+			vim.keymap.set("n", "<leader>d", "<cmd>Neotree toggle<CR>", {
+				noremap = true,
+				silent = true,
+				desc = "Toggle Neo-tree",
+			})
+		end,
+	},
 }
