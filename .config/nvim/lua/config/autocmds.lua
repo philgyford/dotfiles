2ee7aacd99dba5aa4386161f5d5fdeeb8ecf7ac3
Phil Gyford <phil@gyford.com>
2026-04-17 15:27:48 +0100

    Maybe fix the 'return to last position when opening file' stuff
    It wasn't ignore gitcommits, which was annoying, so trying a different
    snippet instead.

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -93,10 +93,46 @@ autocmd("TextYankPost", {
 -- --------------------------------------------------------------------
 -- Go to last location when opening a buffer.
 -- --------------------------------------------------------------------
--- From https://dzx.fr/blog/modern-neovim-config-from-scratch/#treesitter
-autocmd("BufReadPost", {
+-- From https://github.com/neovim/neovim/issues/16339#issuecomment-1348133829
+autocmd({ "BufWinEnter", "FileType" }, {
 	callback = function()
-		local exclude = { "gitcommit" } -- don't remember position in these
+		local ignore_buftype = { "quickfix", "nofile", "help" }
+		local ignore_filetype = { "gitcommit", "gitrebase", "svn", "hgcommit" }
+
+		if vim.tbl_contains(ignore_buftype, vim.bo.buftype) then
+			return
+		end
+
+		if vim.tbl_contains(ignore_filetype, vim.bo.filetype) then
+			-- reset cursor to first line
+			vim.cmd([[normal! gg]])
+			return
+		end
+
+		-- If a line has already been specified on the command line, we are done
+		--   nvim file +num
+		if vim.fn.line(".") > 1 then
+			return
+		end
+
+		local last_line = vim.fn.line([['"]])
+		local buff_last_line = vim.fn.line("$")
+
+		-- If the last line is set and the less than the last line in the buffer
+		if last_line > 0 and last_line <= buff_last_line then
+			local win_last_line = vim.fn.line("w$")
+			local win_first_line = vim.fn.line("w0")
+			-- Check if the last line of the buffer is the same as the win
+			if win_last_line == buff_last_line then
+				-- Set line to last line edited
+				vim.cmd([[normal! g`"]])
+			-- Try to center
+			elseif buff_last_line - last_line > ((win_last_line - win_first_line) / 2) - 1 then
+				vim.cmd([[normal! g`"zz]])
+			else
+				vim.cmd([[normal! G'"<c-e>]])
+			end
+		end
 		local mark = vim.api.nvim_buf_get_mark(0, '"')
 		local lcount = vim.api.nvim_buf_line_count(0)
 		if mark[1] > 0 and mark[1] <= lcount then
