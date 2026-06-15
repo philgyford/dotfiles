400f21c48245c6fc532cb4ac9cbd78e2f76bce69
Phil Gyford <phil@gyford.com>
2026-06-15 15:17:26 +0100

    Tweak ruff/pyright settings

--- .config/nvim/lsp/pyright.lua
+++ .config/nvim/lsp/pyright.lua
@@ -12,8 +12,15 @@ return {
 		".git",
 	},
 	settings = {
+		pyright = {
+			-- Using Ruff's import organizer
+			disableOrganizeImports = true,
+		},
 		python = {
 			analysis = {
+				-- Ignore all files for analysis to exclusively use Ruff for linting
+				ignore = { "*" },
+				-- So not sure if the below does anything...
 				autoSearchPaths = true,
 				diagnosticMode = "openFilesOnly",
 				useLibraryCodeForTypes = true,
